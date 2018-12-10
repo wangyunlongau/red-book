8955dc6a143c408fe83838f3e4d083a7c2994a77
Yunlong Wang <wangyunlongau@gmail.com>
2018-12-10 22:15:38 +1100

    Add answers written in another duplicated project

--- exercises/src/main/scala/fpinscala/datastructures/List.scala
+++ exercises/src/main/scala/fpinscala/datastructures/List.scala
@@ -52,30 +52,30 @@ object List { // `List` companion object. Contains functions for creating and wo
   // Exercise 3.2
   // The function takes constant time
   def tail[A](l: List[A]): List[A] = l match {
-//      case Nil => Nil
-      case Nil => sys.error("tail of empty list")
-      case Cons(_, xs) => xs
+    //      case Nil => Nil
+    case Nil => sys.error("tail of empty list")
+    case Cons(_, xs) => xs
   }
   // Exercise 3.3
   def setHead[A](l: List[A], h: A): List[A] = l match {
-      case Nil => sys.error("head of empty list")
-      case Cons(_, xs) => Cons(h, xs)
+    case Nil => sys.error("head of empty list")
+    case Cons(_, xs) => Cons(h, xs)
   }
   // Exercise 3.4
   def drop[A](l: List[A], n: Int): List[A] =
   {
-//      case Nil => sys.error("empty list")
-      if (n <= 0) l
-      else {
-        l match {
-          case Nil => Nil
-          case Cons(_, t) => drop(t, n - 1)
-        }
+    //      case Nil => sys.error("empty list")
+    if (n <= 0) l
+    else {
+      l match {
+        case Nil => Nil
+        case Cons(_, t) => drop(t, n - 1)
       }
+    }
   }
   // Exercise 3.5
   def dropWhile[A](l: List[A], f: A => Boolean): List[A] = l match {
-//    case Nil => Nil
+    //    case Nil => Nil
     case Cons(h, t) if f(h) => dropWhile(t, f)// Pattern guard using condition before =>
     case _ => l
   }
@@ -86,6 +86,7 @@ object List { // `List` companion object. Contains functions for creating and wo
     case Cons(_, Nil) => Nil
     case Cons(h, t) => Cons(h, init(t))
   }
+  // The answer has another implementation of init which is stack-safe.
   // Exercise 3.7 - Use foldRight to implement product with early termination which is impossible because foldRight
   // traverse the whole list until the end
   def productFoldRight(ds: List[Double]): Double = ds match {
@@ -103,7 +104,7 @@ object List { // `List` companion object. Contains functions for creating and wo
   def length[A](l: List[A]): Int =
     foldRight(l, 0)((_, acc) => acc + 1)
 
-  // Exercise 3.10 ???
+  // Exercise 3.10
   // answer is different from the folding exercises
   def foldLeft[A,B](l: List[A], z: B)(f: (B, A) => B): B = {
     def go(acc: B, l: List[A]): B = l match {
@@ -112,9 +113,60 @@ object List { // `List` companion object. Contains functions for creating and wo
     }
     go(z, l)
   }
-  // Exercise 3.18
+  // Implemented in 1/11/2018
+  // E3.11
+  def sum3(l: List[Int]): Int = foldLeft(l, 0)(_ + _) // implement sum with foldLeft makes it tail-recursive.
+  def product3(l: List[Int]): Int = foldLeft(l, 1)(_ * _)
+  def length3[A](l: List[A]): Int = foldLeft(l, 0)((acc, _) => acc + 1)
+  // E3.12
+  def reverse[A](l: List[A]) = foldLeft(l, List[A]())((acc, h) => Cons(h, acc))
+  // E3.13
+  def foldRightByFoldLeft[A, B](as: List[A], z: B)(f: (A, B) => B) = foldLeft(reverse(as), z)((z, h) => f(h, z))
+  def foldLeftByFoldRight[A, B](as: List[A], z: B)(f: (B, A) => B) = foldRight(reverse(as), z)((h, z) => f(z, h))
+  // E3.14
+  // def appendViaFoldLeft[A](a1: List[A], a2: List[A]): List[A] = foldLeft(a1, a2)((a2, h) => Cons(h, a2))
+  def appendViaFoldLeft[A](a1: List[A], a2: List[A]): List[A] = foldLeft(a1, a2)(Cons(_, _))
+  // foldRight(l, Nil:List[A])(append) is the answer which is not stack-safe.
+  // There is a comment about the solution. What does it mean?
 
+  // E3.15
+  def concat[A](xs: List[List[A]]): List[A] = foldRightByFoldLeft(xs, List[A]())((h, acc) => appendViaFoldLeft(h, acc))
+  // E3.16
+  def addOne(xs: List[Int]) = foldRight(xs, List[Int]())((h, acc) => Cons(h+1, acc))
+  // E3.17
+  def doubleToStringInList(xs: List[Double]): List[String] = foldRight(xs, Nil:List[String])((h, acc) => Cons(h.toString(), acc))
+  // E3.18
+  def map[A,B](l: List[A])(f: A => B): List[B] = foldRight(l, Nil:List[B])((h, acc) => Cons(f(h), acc))
+  // E3.19
+  def filter[A](as: List[A])(f: A => Boolean): List[A] = foldRight(as, Nil:List[A])((h, acc) => if (f(h)) Cons(h, acc) else acc)
+  // E3.20
+  // def flatMap[A,B](as: List[A])(f: A => List[B]): List[B] = foldRight(as, Nil:List[B])((h, acc) => append(f(h), acc))
+  // The answer looks more elegant.
+  def flatMap[A, B](as: List[A])(f: A => List[B]): List[B] = concat(map(as)(f))
+  // Implemented in 2/11/2018
+  // E3.21
+  def filterWithFlatMap[A](as: List[A])(f: A => Boolean): List[A] = flatMap(as)(h => if (f(h)) List(h) else Nil)
+  // At the end of the line, we return Nil as empty list. but when we pass empty list to fold as initial value, we must use nil:List[Int].
+  // E3.22
+  //  def addLists[Int](a: List[Int], b: List[Int]): List[Int] = (a, b) match {
+  //    case (Nil, _) => Nil
+  //    case (_, Nil) => Nil
+  //    case (Cons(h1, t1), Cons(h2, t2)) => Cons(h1+h2, addLists(t1, t2))
+  //  }
+  // Why declaring [Int] gives me problem? How can I solve the problem in a stack-safe way?
+  def addPairwise(a: List[Int], b: List[Int]): List[Int] = (a,b) match {
+    case (Nil, _) => Nil
+    case (_, Nil) => Nil
+    case (Cons(h1,t1), Cons(h2,t2)) => Cons(h1+h2, addPairwise(t1,t2))
+  }
+  // E3.23
+  def zipWith[A,B,C](a: List[A], b: List[B])(f: (A,B) => C): List[C] = (a, b) match {
+    case (Nil, _) => Nil
+    case (_, Nil) => Nil
+    case (Cons(h1, t1), Cons(h2, t2)) => Cons(f(h1, h2), zipWith(t1, t2)(f))
+  }
+  // E3.24
+  def hasSubsequence[A](sup: List[A], sub: List[A]): Boolean = ???
 
-  def map[A,B](l: List[A])(f: A => B): List[B] = ???
-  // Exercise 3.29
+  // E3.29
 }
--- exercises/src/main/scala/fpinscala/datastructures/Tree.scala
+++ exercises/src/main/scala/fpinscala/datastructures/Tree.scala
@@ -6,8 +6,31 @@ case class Branch[A](left: Tree[A], right: Tree[A]) extends Tree[A]
 
 
 object Tree {
-
-
-
-
+  // E3.25
+  def size[A](t: Tree[A]): Int = t match {
+    case Leaf(_) => 1
+    case Branch(l, r) => 1 + size(l) + size(r)
+  }
+  // E3.26
+  def maximum(t: Tree[Int]): Int = t match {
+    case Leaf(a) => a
+    case Branch(l, r) => maximum(l) max maximum(r)
+  }
+  // E3.27
+  def depth[A](t: Tree[A]): Int = t match {
+    case Leaf(_) => 0 // branch -> branch -> lead has path length of 2.
+    case Branch(l, r) => depth(l) max depth(r) + 1
+  }
+  // E3.28
+  def map[A, B](t: Tree[A])(f: A => B): Tree[B] = t match {
+    case Leaf(a) => Leaf(f(a))
+    case Branch(l, r) => Branch(map(l)(f), map(r)(f))
+  }
+  // E3.29 ???
+  // cheat with taking a peek at the function signature of the answer
+  // def fold[A,B](t: Tree[A])(f: A => B)(g: (B,B) => B): B = t match
+  def fold[A, B](t: Tree[A])(f: A => B)(g: (B, B) => B) = t match {
+    case Leaf(a) => f(a)
+    case Branch(l, r) => g()
+  }
 }
